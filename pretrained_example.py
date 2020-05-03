18ea1ea1f2aa79e033a5741e1122afa212389079
tubone <meitantei-conan@live.jp>
2020-05-04 00:11:48 +0900

    Update pretrained_example.py

--- pretrained_example.py
+++ pretrained_example.py
@@ -14,15 +14,16 @@ import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
 import config
+from datetime import datetime
 
 def main():
     # Initialize TensorFlow.
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
-        _G, _D, Gs = pickle.load(f)
+    # Ref: https://www.gwern.net/Faces#anime-faces
+    with open("2019-02-26-stylegan-faces-network-02048-016041.pkl", "rb") as f:
+        _, _, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
         # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
         # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
@@ -40,7 +41,8 @@ def main():
 
     # Save image.
     os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
+    filename = datetime.now().strftime('%Y%m%d%H%M%S') + '.png'
+    png_filename = os.path.join(config.result_dir, filename)
     PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
 
 if __name__ == "__main__":
